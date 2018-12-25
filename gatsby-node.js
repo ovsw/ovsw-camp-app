4e35f1306acfb38a9ec8e829ea3b2689eb575bd2
Ovi Savescu <ovi@ovswebsites.com>
2018-12-25 07:47:29 +0200

    global settings from storyblok

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,7 @@
 // The call to createPage will create a page for each content item we
 // have created in Storyblok when executing gatsby build
 
+const Promise = require('bluebird')
 const path = require('path')
 
 exports.createPages = ({ graphql, actions }) => {
@@ -17,45 +18,81 @@ exports.createPages = ({ graphql, actions }) => {
   return new Promise((resolve, reject) => {
     const storyblokEntry = path.resolve('src/templates/storyblok-entry.js')
 
-    resolve(
-      graphql(
-        `{
-          allStoryblokEntry {
-            edges {
-              node {
-                id
-                name
-                created_at
-                uuid
-                slug
-                full_slug
-                content
-                is_startpage
-                parent_id
-                group_id
-              }
+    // load a configurable site navigation from a global "settings" content item in Storyblok 
+    graphql(
+      `{
+        allStoryblokEntry(
+          filter: {
+            slug: { eq: "settings" }
+          }
+        ) {
+          edges {
+            node {
+              id
+              name
+              created_at
+              published_at
+              uuid
+              slug
+              full_slug
+              content
+              is_startpage
+              parent_id
+              group_id
             }
           }
-        }`
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
         }
+      }`
+    ).then(result => {
+      if (result.errors) {
+        console.log(result.errors)
+        reject(result.errors)
+      }
 
-        const entries = result.data.allStoryblokEntry.edges
-        entries.forEach((entry, index) => {
-          let pagePath = entry.node.full_slug == 'home' ? '' : `${entry.node.full_slug}/`
-
-          createPage({
-            path: `/${pagePath}`,
-            component: storyblokEntry,
-            context: {
-              story: entry.node
+      const globalSettings = result.data.allStoryblokEntry.edges[0].node
+      resolve(
+        graphql(
+          `{
+            allStoryblokEntry {
+              edges {
+                node {
+                  id
+                  name
+                  created_at
+                  published_at
+                  uuid
+                  slug
+                  full_slug
+                  content
+                  is_startpage
+                  parent_id
+                  group_id
+                }
+              }
             }
+          }`
+        ).then(result => {
+          if (result.errors) {
+            console.log(result.errors)
+            reject(result.errors)
+          }
+
+          const entries = result.data.allStoryblokEntry.edges
+          entries.forEach((entry, index) => {
+            
+            let pagePath = entry.node.full_slug == 'home' ? '' : `${entry.node.full_slug}/`
+            
+            createPage({
+              path: `/${pagePath}`,
+              component: storyblokEntry,
+              context: {
+                globalSettings: globalSettings,
+                story: entry.node
+              }
+            })
           })
         })
-      })
-    )
+      )
+    })
   })
 }
