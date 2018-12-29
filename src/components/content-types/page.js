39ebc44194b8f96d44892438daca0314d0862e06
Ovi Savescu <ovi@ovswebsites.com>
2018-12-29 20:24:35 +0200

    added test local gatsby-image

--- src/components/content-types/page.js
+++ src/components/content-types/page.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Components from '../components.js'
+import Image from '../image'
 
 const Page = props => {
   const { blok } = props
+
+  // console.log(blok)
+
   return (
     <div>
-      {blok.body &&
-        blok.body.map(block => React.createElement(Components[block.component], { key: block._uid, blok: block }))}
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <div>
+        {blok.body &&
+          blok.body.map(block => React.createElement(Components[block.component], { key: block._uid, blok: block }))}
+      </div>
     </div>
   )
 }
